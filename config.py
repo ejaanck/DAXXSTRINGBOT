b87af636d6602eb2bf9721c6ea12833956c9d07b
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-06-27 08:10:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "25639252"))
-API_HASH = getenv("API_HASH", "42db0fd56c51ff2b94cf064838eba7c1")
+API_ID = int(getenv("API_ID", "23770828"))
+API_HASH = getenv("API_HASH", "2d3e87f244740e5c8286591940e24cd4")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7121201474:AAGMnBXMtl_quq7h_Dm9wbqMJjtT4Eny1YE")
+BOT_TOKEN = getenv("BOT_TOKEN", "7009082569:AAGtzM7zw0J993cLdxuAFEJaFbreYLf_Kvg")
 #OWNER_ID = int(getenv("6664582540"))
-OWNER_ID = int(getenv("OWNER_ID", "5061180769"))
+OWNER_ID = int(getenv("OWNER_ID", "7149602071"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://jembutan1:1234@cluster0.w186hoa.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "stayheresay")
+MUST_JOIN = getenv("MUST_JOIN", "thebrazzernew")
